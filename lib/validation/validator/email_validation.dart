13ff70721f033a41436a20595c53716e07261cb6
p_souza <psouza@braspag.com.br>
2021-07-29 00:06:46 -0300

    FEAT: add email validation

---
+++ lib/validation/validator/email_validation.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+import '../../presentation/protocol/protocol.dart';
+import '..ion/protocol/protocol.dart';
+
+class EmailValidation extends Equatable implements FieldValidation {
+  EmailValidation(this.field);
+
+  final String field;
+
+  @override
+  ValidationError? validate(Map input) {
+    final regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid =
+        input[field]?.isNotEmpty != true || regex.hasMatch(input[field]);
+    return isValid ? null : ValidationError.invalidField;
+  }
+
+  @override
+  List<Object> get props => [field];
+}
