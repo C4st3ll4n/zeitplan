b1d5108e5af9a2cf15b208d646045f554d5f908d
Pedro Henrique <p13dr0h@gmail.com>
2021-08-06 11:11:38 -0300

    feat: adjust phone entity

--- lib/domain/entity/phone_entity.dart
+++ lib/domain/entity/phone_entity.dart
@@ -3,8 +3,8 @@ import 'package:equatable/equatable.dart';
 class PhoneEntity extends Equatable {
   final String? number, ddd, kind;
 
-  PhoneEntity({required this.number, required this.ddd, this.kind});
+  PhoneEntity({required this.number, this.ddd, this.kind});
 
   @override
-  List<Object?> get props => [number, ddd];
+  List<Object?> get props => [number];
 }
