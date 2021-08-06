32ae061527a550ca2af1fe7b014e681b677d19cd
Pedro Henrique <p13dr0h@gmail.com>
2021-08-06 17:40:22 -0300

    FEAT: adjust prover debug check

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'app.dart';
 
 void main() {
+  Provider.debugCheckInvalidValueType = null;
   runApp(ZeitplanApp());
 }
